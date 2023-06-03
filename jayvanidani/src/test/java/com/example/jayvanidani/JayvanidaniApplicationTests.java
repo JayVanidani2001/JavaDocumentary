3d223053390e60e5049b97ef40f55a5bebdbc590
JayVanidani2001 <jayvanidani2001@gmail.com>
2023-06-03 19:46:10 +0530

    updated README.md

---
+++ jayvanidani/src/test/java/com/example/jayvanidani/JayvanidaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jayvanidani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JayvanidaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
